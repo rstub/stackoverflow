79554aefd3b48133cfd45ad1310092efc0720024
Ralf Stubner <ralf.stubner@daqana.com>
2019-05-24 13:08:56 +0200

    Use thread-local Ziggurat object

--- 56265505/code.cpp
+++ 56265505/code.cpp
@@ -5,22 +5,20 @@
 // [[Rcpp::depends(RcppZiggurat)]]
 #include <Ziggurat.h>
 
-static Ziggurat::Ziggurat::Ziggurat zigg;
 
 using namespace RcppParallel;
 
 struct Norm : public Worker
 {   
-  int input;
-  
   // saved draws
   RVector<double> draws;
   
   // constructors
-  Norm(const int input, Rcpp::NumericVector draws)
-    : input(input), draws(draws) {}
+  Norm(Rcpp::NumericVector draws)
+    : draws(draws) {}
   
   void operator()(std::size_t begin, std::size_t end) {
+    Ziggurat::Ziggurat::Ziggurat zigg(end);
     for (std::size_t i = begin; i < end; i++) {
       draws[i] = zigg.norm();
     }
@@ -29,35 +27,25 @@ struct Norm : public Worker
 
 // [[Rcpp::export]]
 Rcpp::NumericVector parallelDraws(int x) {
-  
   // allocate the output vector
-  Rcpp::NumericVector draws(x);
-  
-  // declare the Sum instance 
-  Norm norm(x, draws);
-  
-  // call parallelFor to start the work
+  Rcpp::NumericVector draws(Rcpp::no_init(x));
+  Norm norm(draws);
   parallelFor(0, x, norm);
-  
-  // return the draws
   return draws;
-};
+}
 
 // [[Rcpp::export]]
 Rcpp::NumericVector serialDraws(int x) {
-  
   // allocate the output vector
-  Rcpp::NumericVector draws(x);
-  
+  Rcpp::NumericVector draws(Rcpp::no_init(x));
+  Ziggurat::Ziggurat::Ziggurat zigg(42);
   for (int i = 0; i < x; i++) {
     draws[i] = zigg.norm();
   }
-  
-  // return the draws
   return draws;
-};
+}
 
 /*** R
-library(microbenchmark)
-microbenchmark(parallelDraws(1e5), serialDraws(1e5), rnorm(1e5))
+bench::mark(parallelDraws(1e5), serialDraws(1e5), check = FALSE, min_iterations = 10)[,1:5]
+bench::mark(parallelDraws(1e8), serialDraws(1e8), check = FALSE, min_iterations = 10)[,1:5]
 */
